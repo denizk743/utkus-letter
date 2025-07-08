271ac610b3ab455e3c878669b0e3550ef03e452e
denizk743 <164668314+denizk743@users.noreply.github.com>
2025-07-08 20:40:11 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const envelope = document.getElementById('envelope');
 
-  envelope.addEventListener('click', () => {
-    envelope.classList.toggle('open');
-  });
-});
